82d89640560fbbac21babe01710e9c4b3b425172
shengsheng <szhou006@e.ntu.edu.sg>
2018-01-31 16:04:55 +0800

    Update activemq test class

--- src/main/java/com/dream/dw/mq/activemq/test/Consumer.java
+++ src/main/java/com/dream/dw/mq/activemq/test/Consumer.java
@@ -1,10 +1,14 @@
 package com.dream.dw.mq.activemq.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
+
+    Logger logger = LoggerFactory.getLogger(Consumer.class);
 
     /**
      * Listener to listen mytest.queue
@@ -12,7 +16,7 @@ public class Consumer {
      */
     @JmsListener(destination = "test.queue")
     public void myTestQueueListener(String message) {
-        System.out.println("Received message from test.queue:" + message);
+        logger.error("Received message from test.queue: " + message);
     }
 
 }
